46695f36c830b47140cb06b630af8422106a3feb
Esdras Pinheiro <eps.94@hotmail.com>
2019-10-31 22:10:45 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+function App() {
+    const [tech, setTech] = useState(['ReactJS', 'React Native']);
+    const [newTech, setNewTech] = useState('');
+
+    const handleAdd = useCallback(() => {
+        setTech([...tech, newTech]);
+        setNewTech('');
+    }, [newTech, tech]);
+
+    useEffect(() => {
+        const storageTech = localStorage.getItem('tech');
+
+        if (storageTech) {
+            setTech(JSON.parse(storageTech));
+        }
+
+        return () => {}; // linha usada para executar quando o componente deixar de existir
+    }, []); // Vazio para executar uma unica vez
+
+    useEffect(() => {
+        localStorage.setItem('tech', JSON.stringify(tech));
+    }, [tech]);
+
+    const techSize = useMemo(() => tech.length, [tech]);
+
+    return (
+        <>
+            <ul>
+                {tech.map(t => (
+                    <li key={t}>{t}</li>
+                ))}
+            </ul>
+            <strong>Você tem {techSize} tecnologias</strong>
+            <br />
+            <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+            <button type="button" onClick={handleAdd}>
+                Adicionar
+            </button>
+        </>
+    );
+}
+
+export default App;
